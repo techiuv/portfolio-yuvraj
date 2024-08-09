ae8e9c38f454a9fb9831224ed4ca9b4773f6348c
Yuvraj <160020119+yuvraj1703@users.noreply.github.com>
2024-08-09 15:32:34 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
         strings: ['Professional Artist', 'Web Developer','Art Mentor'],
         typeSpeed: 50,
         loop: true
-    });
+    }); 
 
     let navLinks = document.querySelectorAll(".nav-link");
     let offcanvas = document.getElementById("offcanvasNavbar");
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", function () {
             offcanvasInstance.hide();
 
             // Manually navigate to the target section
-            var target = link.getAttribute("href");
+            let target = link.getAttribute("href");
             document.querySelector(target).scrollIntoView({
                 behavior: 'smooth'
             });
@@ -24,7 +24,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Add shadow to navbar on scroll
     window.addEventListener("scroll", function () {
-        var navbar = document.querySelector(".navbar");
+        let navbar = document.querySelector(".navbar");
         if (window.scrollY > 50) {
             navbar.classList.add("shadow");
         } else {
@@ -36,30 +36,48 @@ document.addEventListener("DOMContentLoaded", function () {
 document.getElementById('year').innerHTML = '&copy;' + new Date().getFullYear() + '. All rights reserved.';
 
 
-gsap.from(".pic, #home h3,#home h3 span, #home h5 #home p, .social-links", {
+gsap.from(".pic, #home h3,#home h3 span, #home h5, #home p, .social-links", {
     y:20,
     opacity: 0,
-    duration: 2,
+    duration: .5,
+    stagger:.3    
 });
 
 gsap.from("#about h3, #about p", {
     opacity: 0,
     y: 10,
-    
-    duration: 1,
+    stagger:.5,
+    duration: .5,
     scrollTrigger: {
         trigger: "#about",
         start: "top 80%", 
         
         
     }
-});
+}); 
+
+// Assuming you have included the SplitText plugin
+gsap.fromTo("#about h3 ", 
+    {
+        clipPath: "inset(100% 0% 0% 0%)" // Starts with the element fully hidden from the bottom
+    }, 
+    {
+        clipPath: "inset(0% 0% 0% 0%)", // Ends with the element fully visible
+        opacity: 1, 
+        duration: .5,
+        scrollTrigger: {
+            trigger: "#about",
+            start: "top 80%", 
+            toggleActions: "play none none none"
+        }
+    }
+);
 
 
 gsap.from("#work h3, #work p", {
     opacity: 0,
     y: 10,
-    
+    stagger:.5,
     duration: 1,
     scrollTrigger: {
         trigger: "#work",
@@ -70,10 +88,11 @@ gsap.from("#work h3, #work p", {
 });
 
 gsap.from(".box", {
-    y: 10,
+    y: 50,
     opacity:0,
-    scale:0,
+    //scale:0,
     duration: 1,
+    stagger:1,
     scrollTrigger: {
         trigger: "#work",
         start: "top 80%", 
@@ -85,7 +104,7 @@ gsap.from(".box", {
 gsap.from("#contact h3, #contact p", {
     opacity: 0,
     y: 10,
-    
+    stagger:.5,
     duration: 1,
     scrollTrigger: {
         trigger: "#contact",
@@ -95,9 +114,11 @@ gsap.from("#contact h3, #contact p", {
 });
 
 
-gsap.from("#contact div", {
+gsap.from("form", {
     opacity: 0,
-    scale:0,
+    y: 10,
+   // flip:180,
+    stagger: .5,
     duration: 1,
     scrollTrigger: {
         trigger: "#contact",
@@ -105,3 +126,5 @@ gsap.from("#contact div", {
 
     }
 });
+
+
